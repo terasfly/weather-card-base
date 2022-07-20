041b8b2923c76647f4a124d894cf709e93462caa
terasfly <84103159+terasfly@users.noreply.github.com>
2022-07-20 07:51:36 +0100

    Add files via upload

---
+++ juodrastis.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i < day.length; i++) {
+//     document.querySelectorAll('.day')[i].addEventListener('click', function() {
+//         this.style.visibility = 'hidden'
+// const btnE2 = document.querySelectorAll('.knopke1')
+
+
+
+// console.log(event.pageY)
+// for (let i = 0; i < btnE1.length; i++) {
+// const x = (event.pageX - btnE1.offsetLeft)
+// const y = (event.pageY - btnE1.offsetTop);
+
+// btnE1.style.setProperty('--xPos', x + 'px')
+// btnE1.style.setProperty('--yPos', y + 'px')
